98832a0a67d459869d404bcc9413392cc04d52ff
Chinmay Garg <chinmaygarg@me.com>
2019-05-22 12:00:38 -0700

    trying something with .device

--- train.py
+++ train.py
@@ -79,7 +79,7 @@ def train(max_length, model_size, epochs, learning_rate, device, num_heads, num_
             for j, b in enumerate(iter(tqdm(train))):
                 optimizer.zero_grad()
                 # print('\nreview is \n\n', b.review[0])
-                model_out = model(b.review[0].to(device))
+                model_out = model(b.review[0].device(device))
                 # calculate loss
                 # print('\nrating is \n\n', b.rating)
                 loss = criterion(model_out, b.rating.to(device))
