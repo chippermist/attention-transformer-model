0ec337cc9897f1e66a20321df4d870558c249c39
Chinmay Garg <chinmaygarg@me.com>
2019-05-21 19:15:55 -0700

    Update train.py

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ def val(model, test, vocab, device, epoch_num, path_saving):
     with torch.no_grad():
         correct = 0.0
         total = 0.0
-        for b, i in enumerate(tqdm(test)):
+        for b, i in enumerate(iter(tqdm(test))):
             if not vis is None:
                 visdom_windows = plot_weights(model, visdom_windows, b, vocab, vis)
             model_out = model(b.review[0].to(device)).to("cpu").numpy()
